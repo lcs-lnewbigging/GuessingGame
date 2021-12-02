b1f0f7875e8a867705325a57ad79a0d7cb233686
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-02 09:09:03 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Luke Newbigging on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
